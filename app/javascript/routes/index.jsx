59a9e61b007932c0a9ee1d06d5666f63580d4ec6
EmirVatric <vatricemir@gmail.com>
2019-11-16 13:22:38 +0100

    navbar

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -3,9 +3,74 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Book from "../components/books";
 
 export default (
-  <Router>
-    <Switch>
-      <Route path="/" exact component={Book} />
-    </Switch>
-  </Router>
+  <div>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          BookSTORE
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                SignUp/Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Logout
+              </a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Book} />
+        </Switch>
+      </Router>
+    </div>
+  </div>
 );
