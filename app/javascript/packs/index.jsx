410e9e641f6b6f10ab2d70b148cf0083e5512afd
EmirVatric <vatricemir@gmail.com>
2019-11-21 12:44:10 +0100

    simple redux setup;

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -5,7 +5,20 @@ import $ from "jquery";
 import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "../components/app";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "../reducers/rootReducer";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 document.addEventListener("DOMContentLoaded", () => {
-  render(<App />, document.body.appendChild(document.createElement("div")));
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.body.appendChild(document.createElement("div"))
+  );
 });
---
+++ app/javascript/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+const initState = {
+  loggedin: false
+}
+
+const rootReducer = (state = initState, action) => {
+  return state
+}
+
+export default rootReducer
